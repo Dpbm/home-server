27fcb2730705d2f0ba3103d30713936c7c90c191
Dpbm <dpbm136@gmail.com>
2024-07-04 11:55:23 -0300

    updated configs

--- setup.sh
+++ setup.sh
@@ -8,11 +8,15 @@ log(){
   echo -e "$GREEN$text$END"
 }
 
+
+
 log "Updating Everything..."
 git clone https://github.com/Dpbm/update-all $HOME/update-all
 echo 'alias update-all="$\HOME/udpate-all/update-all"' >> $HOME/.bashrc
 source $HOME/.bashrc
 update-all
+
+
 
 
 log "Installing Docker..."
@@ -30,9 +34,13 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
+
+
 log "Testing Docker..."
 sudo docker run hello-world
 sudo docker container prune && sudo docker image prune -a
+
+
 
 
 log "Changing Docker Users Permissions..."
@@ -41,17 +49,53 @@ sudo usermod -aG docker $USER
 newgrp docker
 
 
+
+
 log "Setup Docker to Start on Startup..."
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 
 
+
 log "Starting Containers..."
 docker compose up -d
+ 
+
+log "Setup static IP..."
+echo "the new ip: "
+read $new_ip
+sudo su
+echo "interface wlan0" >> /etc/dhcpcd.conf
+echo "static ip_address=$new_ip/24" >> /etc/dhcpcd.conf
+echo "static routers=192.168.0.1" >> /etc/dhcpcd.conf
+echo "static domain_name_servers=192.168.0.1 1.1.1.1" >> /etc/dhcpcd.conf
+exit
+
+
+
+log "Activating VNC..."
+sudo ln /usr/lib/systemd/system/vncserver-x11-serviced.service /etc/systemd/system/multi-user.target.wants/vncserver-x11-serviced.service
+sudo systemctl start vncserver-x11-serviced
+vncpasswd -user
+
+
+log "Donwloading final dependecies..."
+sudo apt install rkhunter
+git clone https://github.com/goodtft/LCD-show $HOME
+
+
+log "Disable Bluetooth..."
+echo "dtoverlay=disable-bt" >> $HOME/LCD-show/boot/config.txt.bak
+
+
+
+log "Creating Projects folder (for theia-IDE)..."
+mkdir -p $HOME/projects/
+
+
 
 log "Setting up the 3.2inch Tft Screen..."
 log "YOUR SYSTEM IS GOING TO RESTART AFTER THAT"
-git clone https://github.com/goodtft/LCD-show $HOME
 mkdir old-boot-config
 sudo cp /boot/config.txt ./old-boot-config
 cd $HOME/LCD-show
